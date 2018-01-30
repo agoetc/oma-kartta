5f0f449c57985245759e7e8052819cd0b0d8e85c
agoetc <b6151@oic.jp>
2018-01-30 18:16:58 +0900

    docker-compose file add

---
+++ mysql/init.d/setup.sql
@@ -0,0 +1,34 @@
+grant all privileges on *.* to user@'%' IDENTIFIED BY 'Password' WITH GRANT OPTION;
+
+CREATE TABLE users (
+  id VARCHAR(255) NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  password VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE user_restaurants (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_id VARCHAR(255) NOT NULL,
+  star int(2),
+  sana VARCHAR(255),
+  latitude DECIMAL(9,6) NOT NULL,
+  longitude DECIMAL(9,6) NOT NULL,
+  created_at timestamp not null default current_timestamp,
+  updated_at timestamp not null default current_timestamp on update current_timestamp
+);
+
+CREATE TABLE user_reviews (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  user_id VARCHAR(255) NOT NULL,
+  review VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE restaurants (
+  id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+  name VARCHAR(255) NOT NULL,
+  kana VARCHAR(255) NOT NULL,
+  phone INTEGER(12) NOT NULL,
+  text VARCHAR(255),
+  postal_code INTEGER(8) NOT NULL,
+  address VARCHAR(255) NOT NULL
+);

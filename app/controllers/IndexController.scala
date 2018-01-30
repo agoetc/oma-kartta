db85c17f8207b1b13a756139c924168d97627453
agoetc <b6151@oic.jp>
2018-01-30 19:28:46 +0900

    IndexController routing done

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -4,11 +4,25 @@ import javax.inject._
 import play.api.mvc._
 
 @Singleton
-class IndexController @Inject()(cc: ControllerComponents) extends AbstractController(cc)  {
+class IndexController @Inject()(cc: ControllerComponents) extends AbstractController(cc){
 
   def index = Action{
     Ok(views.html.index())
   }
 
+  def signin = Action{
+    Ok(views.html.signin())
+  }
 
+  def signup = Action{
+    Ok(views.html.signin())
+  }
+
+  def signout = Action{
+    Redirect(routes.IndexController.index())
+  }
+
+  def main =  Action{
+    Ok(views.html.main())
+  }
 }

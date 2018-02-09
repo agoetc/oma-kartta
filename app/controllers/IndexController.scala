d0352ec24f38d216b660d021143ab3f5118b2d7d
agoetc <b6151@oic.jp>
2018-02-09 23:12:02 +0900

    login code fix

--- app/controllers/IndexController.scala
+++ app/controllers/IndexController.scala
@@ -35,12 +35,12 @@ class IndexController @Inject()(cc: ControllerComponents) extends AbstractContro
       "password" -> nonEmptyText
     )(AuthForm.apply)(AuthForm.unapply))
 
+  // 入力されたuserを検索、存在すればtrueを返す
   def userCheck(form: AuthForm,request: Request[AnyContent]):Boolean ={
     val db = Database.forConfig("mysqldb")
     val user = db.run(Users.filter(user => user.id === form.id && user.password === form.password).result)
     Await.ready(user, 20 second)
-    println(user.value.get.get.isEmpty)
-    user.value.get.get.isEmpty
+    user.value.get.get.nonEmpty
   }
 
   def signincheck = Action { implicit request =>
@@ -48,8 +48,8 @@ class IndexController @Inject()(cc: ControllerComponents) extends AbstractContro
       errors => Ok(views.html.signin()),
       form =>{
         userCheck(form,request) match {
-          case false => Redirect("/main").withSession ("user_id" -> form.id)
-          case true => Redirect("/signin").flashing("error" -> "")
+          case true => Redirect("/main").withSession ("user_id" -> form.id)
+          case false => Redirect("/signin").flashing("error" -> "")
         }
       }
     )

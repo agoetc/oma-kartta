0a7e3ae9af7c6da3a825e9fc77bf53d86dea2f73
agoetc <b6151@oic.jp>
2018-05-20 19:50:18 +0900

    [修正] バリデーション修正、パイッカ詳細、改行できるようにした

--- app/forms/PaikkaForm.scala
+++ app/forms/PaikkaForm.scala
@@ -21,10 +21,10 @@ object PaikkaForm {
 
   val createForm = Form(
     mapping(
-      "name" -> nonEmptyText,
-      "kana" -> nonEmptyText,
-      "tag" -> nonEmptyText,
-      "text" -> optional(text),
+      "name" -> nonEmptyText(maxLength = 20),
+      "kana" -> nonEmptyText(maxLength = 30),
+      "tag" -> nonEmptyText(maxLength = 20),
+      "text" -> optional(text(maxLength = 255)),
       "postal_code" -> nonEmptyText,
       "address" -> nonEmptyText
     )(CreatePaikkaForm.apply)(CreatePaikkaForm.unapply)
